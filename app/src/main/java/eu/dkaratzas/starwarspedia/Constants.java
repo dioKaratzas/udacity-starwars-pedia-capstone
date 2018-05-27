228972d13f37893f7e1df3a04396d14304b0a4e7
Dionysios Karatzas <dnkaratzas@gmail.com>
2018-05-27 19:22:11 +0300

    Added Copyrights

--- app/src/main/java/eu/dkaratzas/starwarspedia/Constants.java
+++ app/src/main/java/eu/dkaratzas/starwarspedia/Constants.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Dionysios Karatzas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.dkaratzas.starwarspedia;
 
 public class Constants {
@@ -16,6 +32,6 @@ public class Constants {
         if (BuildConfig.DEBUG)
             return "ca-app-pub-3940256099942544/6300978111";
 
-        return "ca-app-pub-7937197470485695~8245374691";
+        return "ca-app-pub-7937197470485695/4744704266";
     }
 }
