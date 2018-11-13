ea56fb37ac6a7da6519c73364129d6e7625b2b46
Dionisis Karatzas <dnkaratzas@gmail.com>
2018-11-13 13:25:50 +0200

    Added Privacy & Policy and T&C to comply with Developer Distribution Agreement regarding Violation of Usage of Android Advertising ID policy

--- app/src/main/java/eu/dkaratzas/starwarspedia/controllers/activities/MainActivity.java
+++ app/src/main/java/eu/dkaratzas/starwarspedia/controllers/activities/MainActivity.java
@@ -31,6 +31,8 @@ import android.widget.FrameLayout;
 
 import com.squareup.leakcanary.RefWatcher;
 
+import net.khirr.android.privacypolicy.PrivacyPolicyDialog;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import eu.dkaratzas.starsgl.widget.StarView;
@@ -73,7 +75,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     // region Activity Lifecycle
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
@@ -93,19 +95,30 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         mNavView.setNavigationItemSelectedListener(this);
         mNavView.getMenu().findItem(R.id.nav_premium).setVisible(InAppBillingManager.isDisplayAds());
 
-        Bundle bundle = getIntent().getExtras();
+        final Bundle bundle = getIntent().getExtras();
 
         // If turn the screen orientation then the savedInstanceState is not null.
         // In this condition, do not need to add new fragment again.
-        if (savedInstanceState == null) {
-            if (bundle != null && bundle.containsKey(EXTRA_FAVOURITE_DATA)) {
-                loadItemDetailsAndLaunchActivity((SimpleQueryData) bundle.getParcelable(EXTRA_FAVOURITE_DATA), getString(R.string.favourites));
-                mNavView.setCheckedItem(R.id.nav_favourites);
-                switchToFavouritesCategory();
-            } else {
-                selectDefaultCategory();
+        showPrivacyPolicyDialog(new PrivacyPolicyDialog.OnClickListener() {
+            @Override
+            public void onAccept(boolean b) {
+                if (savedInstanceState == null) {
+                    if (bundle != null && bundle.containsKey(EXTRA_FAVOURITE_DATA)) {
+                        loadItemDetailsAndLaunchActivity((SimpleQueryData) bundle.getParcelable(EXTRA_FAVOURITE_DATA), getString(R.string.favourites));
+                        mNavView.setCheckedItem(R.id.nav_favourites);
+                        switchToFavouritesCategory();
+                    } else {
+                        selectDefaultCategory();
+                    }
+                }
             }
-        }
+
+            @Override
+            public void onCancel() {
+                finish();
+            }
+        });
+
     }
 
     @Override
@@ -328,6 +341,25 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             loadingDialog.dismiss();
             loadingDialog = null;
         }
+    }
+
+    private void showPrivacyPolicyDialog(PrivacyPolicyDialog.OnClickListener listener) {
+        PrivacyPolicyDialog dialog = new PrivacyPolicyDialog(this,
+                "https://dnkaratzas.github.io/udacity-starwars-pedia-capstone/terms_and_conditions",
+                "https://dnkaratzas.github.io/udacity-starwars-pedia-capstone/privacy_policy");
+
+        dialog.addPoliceLine("By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Dionysis Karatzas.");
+        dialog.addPoliceLine("Dionysis Karatzas is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.");
+        dialog.addPoliceLine("The StarWars Pedia app stores and processes personal data that you have provided to us, in order to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the StarWars Pedia app won’t work properly or at all.");
+        dialog.addPoliceLine("You should be aware that there are certain things that Dionysis Karatzas will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Dionysis Karatzas cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.");
+        dialog.addPoliceLine("If you’re using the app outside of an area with Wi-Fi, you should remember that your terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.");
+        dialog.addPoliceLine("Along the same lines, Dionysis Karatzas cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, Dionysis Karatzas cannot accept responsibility.");
+        dialog.addPoliceLine("With respect to Dionysis Karatzas’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. Dionysis Karatzas accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.");
+        dialog.addPoliceLine("At some point, we may wish to update the app. The app is currently available on Android – the requirements for system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Dionysis Karatzas does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.");
+
+        dialog.setOnClickListener(listener);
+
+        dialog.show();
     }
 
     // endregion
